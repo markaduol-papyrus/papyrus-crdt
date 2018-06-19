95e7f0ffbed6237d0b8ec79e8a5d887566982870
Mark Aduol <mark.aduol15@imperial.ac.uk>
2018-06-19 01:31:27 +0100

    WIP: Working on remote insertion unit tests - 1 such test failing

--- test/crdt.test.js
+++ test/crdt.test.js
@@ -1,7 +1,12 @@
 import CRDT from './../lib/crdt.js';
+import Identifier from './../lib/identifier.js';
+import Char from './../lib/char.js';
 
-function _getRandomInt() {
-  return Math.floor(Math.random() * 1000);
+/**
+ * Get random integer in interval [0, hi)
+ */
+function _getRandomInt(minValue=0, maxValue=1000) {
+  return minValue + Math.floor(Math.random() * maxValue);
 }
 
 function _getRandomChar() {
@@ -19,8 +24,56 @@ function _getRandomChars(n) {
   return res;
 }
 
+function _getRandomIdArray() {
+  const [minLength, maxLength] = [1, 10];
+  const length = _getRandomInt(minLength, maxLength + 1);
+  const siteId = _getRandomInt();
+
+  // `2 ** rootLog2Base` gives branching factor at root of tree
+  const [minLog2Base, maxLog2Base] = [1, 10];
+  const rootLog2Base = _getRandomInt(minLog2Base, maxLog2Base + 1);
+
+  let idArray = [];
+  for (let i = 0; i < length; i++) {
+    let value = _getRandomInt(2 ** (rootLog2Base + i));
+    let id = new Identifier(value, siteId);
+    idArray.push(id);
+  }
+  return idArray;
+}
+
+function _getDistinctIdArrays(n) {
+  let idArrays = new Set();
+  let i = 0
+  while (i < n) {
+    let idArr = _getRandomIdArray();
+    if (!(idArrays.has(idArr))) {
+      idArrays.add(idArr);
+      i++;
+    }
+  }
+  return Array.from(idArrays);
+}
+
+function _compareIdArrays(idArr1, idArr2) {
+  let length1 = idArr1.length;
+  let length2 = idArr2.length;
+  for (let i = 0; i < Math.min(length1, length2); i++) {
+    let comparison = idArr1[i].compareTo(idArr2[i]);
+    if (comparison !== 0) return comparison;
+  }
+  // Break prefix ties on length of ID
+  if (length1 < length2) {
+    return -1;
+  } else if (length1 === length2) {
+    return 0;
+  } else {
+    return 1;
+  }
+}
+
 /////////////////////////////// ACTUAL TESTS ///////////////////////////////////
-describe('Insertion tests (with randomization)', () => {
+describe('Local insertion tests (with randomization)', () => {
   let crdt;
 
   beforeEach(() => {
@@ -116,15 +169,141 @@ describe('Insertion tests (with randomization)', () => {
   });
 
   test('Local insertions of (character -> newline -> character)', async () => {
+    const pos1 = {lineIndex: 0, charIndex: 0};
+    const pos2 = {lineIndex: 0, charIndex: 1};
+    const pos3 = {lineIndex: 1, charIndex: 0};
+    const randChars = _getRandomChars(2);
+    const charObj1 = await crdt.handleLocalInsert(randChars[0], pos1);
+    const charObj2 = await crdt.handleLocalInsert('\n', pos2);
+    const charObj3 = await crdt.handleLocalInsert(randChars[1], pos3);
+    // Ordering checks
+    expect(charObj1.compareTo(charObj2)).toEqual(-1);
+    expect(charObj2.compareTo(charObj3)).toEqual(-1);
+    // Array checks
+    expect(crdt.lineArray[0][0]).toEqual(charObj1);
+    expect(crdt.lineArray[0][1]).toEqual(charObj2);
+    expect(crdt.lineArray[1][0]).toEqual(charObj3);
+  });
+});
 
+describe('Remote insertion tests (with randomization)', () => {
+  let crdt;
+
+  beforeEach(() => {
+    crdt = new CRDT(_getRandomInt());
   });
 
-  test('Local insertions of (character -> double newline)', async () => {
+  test('Remote insertion of random character', async () => {
+    const idArr = _getRandomIdArray();
+    const charObj = new Char(_getRandomChar(), idArr);
+    const expectedInsertPos = {lineIndex: 0, charIndex: 0};
 
+    // Get returned character object and position at which insertion occurred.
+    let retCharObj, insertPos;
+    [retCharObj, insertPos] = await crdt.handleRemoteInsert(charObj);
+    // Value check
+    expect(charObj).toEqual(retCharObj);
+    // Position check
+    expect(insertPos).toEqual(expectedInsertPos);
   });
 
-  test('Local insertions of (character -> double newline -> character)',
-       async () => {
+  test('Remote insertion of (character -> character)', async () => {
+    const idArrays = _getDistinctIdArrays(2);
+    const compValue = _compareIdArrays(idArrays[0], idArrays[1]);
+    const charObj1 = new Char(_getRandomChar(), idArrays[0]);
+    const charObj2 = new Char(_getRandomChar(), idArrays[1]);
 
+    let [retCharObj1, insertPos1] = await crdt.handleRemoteInsert(charObj1);
+    let [retCharObj2, insertPos2] = await crdt.handleRemoteInsert(charObj2);
+    // Value check
+    expect(charObj1).toEqual(retCharObj1);
+    expect(charObj2).toEqual(retCharObj2);
+    // Position check
+    const pos1 = {lineIndex: 0, charIndex: 0};
+    const pos2 = {lineIndex: 0, charIndex: 1};
+    if (compValue < 0) {
+      expect(insertPos1).toEqual(pos1);
+      expect(insertPos2).toEqual(pos2);
+    } else {
+      expect(insertPos1).toEqual(pos2);
+      expect(insertPos2).toEqual(pos1);
+    }
   });
+
+  test('Remote insertion of (newline -> newline)', async () => {
+    const idArrays = _getDistinctIdArrays(2);
+    const compValue = _compareIdArrays(idArrays[0], idArrays[1]);
+    const charObj1 = new Char('\n', idArrays[0]);
+    const charObj2 = new Char('\n', idArrays[1]);
+
+    let [retCharObj1, insertPos1] = await crdt.handleRemoteInsert(charObj1);
+    let [retCharObj2, insertPos2] = await crdt.handleRemoteInsert(charObj2);
+    // Value check
+    expect(charObj1).toEqual(retCharObj1);
+    expect(charObj2).toEqual(retCharObj2);
+    // Position check
+    const pos1 = {lineIndex: 0, charIndex: 0};
+    const pos2 = {lineIndex: 1, charIndex: 0}; // since prev. char is newline
+    if (compValue < 0) {
+      expect(insertPos1).toEqual(pos1);
+      expect(insertPos2).toEqual(pos2);
+    } else {
+      expect(insertPos1).toEqual(pos2);
+      expect(insertPos2).toEqual(pos1);
+    }
+  });
+
+  test('Remote insertion of (character -> newline)', async () => {
+    const idArrays = _getDistinctIdArrays(2);
+    const compValue = _compareIdArrays(idArrays[0], idArrays[1]);
+    if (compValue > 0) {
+      let temp = idArrays[0];
+      idArrays[0] = idArrays[1];
+      idArrays[1] = temp;
+    }
+    const charObj1 = new Char(_getRandomChar(), idArrays[0]);
+    const charObj2 = new Char('\n', idArrays[1]);
+
+    let [retCharObj1, insertPos1] = await crdt.handleRemoteInsert(charObj1);
+    let [retCharObj2, insertPos2] = await crdt.handleRemoteInsert(charObj2);
+    // Value check
+    expect(charObj1).toEqual(retCharObj1);
+    expect(charObj2).toEqual(retCharObj2);
+    // Position check
+    const pos1 = {lineIndex: 0, charIndex: 0};
+    const pos2 = {lineIndex: 0, charIndex: 1};
+    expect(insertPos1).toEqual(pos1);
+    expect(insertPos2).toEqual(pos2);
+  });
+
+  test('Remote insertion of (newline -> character)', async () => {
+    const idArrays = _getDistinctIdArrays(2);
+    const compValue = _compareIdArrays(idArrays[0], idArrays[1]);
+    if (compValue > 0) {
+      let temp = idArrays[0];
+      idArrays[0] = idArrays[1];
+      idArrays[1] = temp;
+    }
+    const charObj1 = new Char('\n', idArrays[0]);
+    const charObj2 = new Char(_getRandomChar(), idArrays[1]);
+
+    let [retCharObj1, insertPos1] = await crdt.handleRemoteInsert(charObj1);
+    let [retCharObj2, insertPos2] = await crdt.handleRemoteInsert(charObj2);
+    // Value check
+    expect(charObj1).toEqual(retCharObj1);
+    expect(charObj2).toEqual(retCharObj2);
+    // Position check
+    const pos1 = {lineIndex: 0, charIndex: 0};
+    const pos2 = {lineIndex: 1, charIndex: 0}; // since prev. char is newline
+    expect(insertPos1).toEqual(pos1);
+    expect(insertPos2).toEqual(pos2);
+  });
+});
+
+describe('Local deletion tests (with randomization)', () => {
+
+});
+
+describe('Remote deletion tests (with randomization)', () => {
+
 });
